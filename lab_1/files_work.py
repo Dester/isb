3f5064795a2b478caba949f79a10aa2a2fb69bea
Dester <ilanosov160@gmail.com>
2025-03-31 12:10:30 +0400

    has been created library for working with txt and json files

---
+++ lab_1/files_work.py
@@ -0,0 +1,37 @@
+import json
+
+
+def read_txt_file(filename: str) -> str:
+    """
+    Считывает данные из txt файла
+    :param filename: название файла в формате txt
+    :return: считанные данные из файла
+    """
+    if filename is not None:
+        with open(filename, "r", encoding="utf-8") as file:
+            return file.read()
+    else:
+        raise FileNotFoundError("File not found")
+
+
+def write_txt_file(filename: str, text: str) -> None:
+    """
+    Записывает данные в txt файл
+    :param filename: название файла в формате txt
+    :param text: данные для записи в файл
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(text)
+
+
+def read_json_file(filename: str) -> str:
+    """
+    Считывает данные из json файла
+    :param filename: название файла в формате json
+    :return: считанные данные из файла
+    """
+    if filename is not None:
+        with open(filename) as file:
+            return json.load(file)
+    else:
+        raise FileNotFoundError("File not found")
